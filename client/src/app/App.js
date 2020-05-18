7e9c2cebdfb3b94445bc4915bb36600ade38a7d3
jona506a <jobu@live.dk>
2020-05-18 17:41:15 +0200

    login start

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import classes from './App.module.css';
+import Login from './components/Login/Login'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className={classes.App}>
-      <h1>Hello HolidayHouse!</h1>
-      <img src="https://holidayhouse1.s3.amazonaws.com/default.jpeg" alt='s3-img' />
-    </div>
+    // <div className={classes.App}>
+    //   <h1>Hello HolidayHouse!</h1>
+    //   <img src="https://holidayhouse1.s3.amazonaws.com/default.jpeg" alt='s3-img' />
+    // </div>
+  
+    <BrowserRouter>
+    <main>
+      <Switch>
+        <Route path="/login" component={Login} exact />
+      </Switch>
+    </main>
+  </BrowserRouter>  
   );
 }
 
