2fd17364f40e53dd6e284a0355fa4117498d87c8
Sebastian Rotar <rotar.seby1@gmail.com>
2020-06-03 17:32:22 +0200

    fixed link

--- client/src/app/helpers/auth.js
+++ client/src/app/helpers/auth.js
@@ -1,5 +1,5 @@
-// export const endpoint = 'http://ec2-13-48-148-107.eu-north-1.compute.amazonaws.com:5555/api/';
-export const endpoint = 'http://localhost:5555/api/';
+export const endpoint = 'http://ec2-13-48-148-107.eu-north-1.compute.amazonaws.com:5555/api/';
+// export const endpoint = 'http://localhost:5555/api/';
 
 const usersEndpoint = endpoint + '/users';
 
